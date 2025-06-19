6dcb808939c6a195f9e53f7af8053436de68e2d2
pawandeepsingh <singhpawandeep8094@gmail.com>
2025-06-19 10:41:40 +0530

    initial commit

---
+++ src/main/java/com/brar/UdemyMultithreading/UdemyMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.brar.UdemyMultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyMultithreadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brar/UdemyMultithreading/UdemyMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brar.UdemyMultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
